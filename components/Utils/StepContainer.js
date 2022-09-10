947162547e433aafbf14c91b8489c325769a0375
J.A. Aartsen <jamal.aartsen@wearetriple.com>
2022-09-10 14:43:12 +0200

    made steps section responsive

--- components/Utils/StepContainer.js
+++ components/Utils/StepContainer.js
@@ -1,24 +1,33 @@
 import Styles from "../../styles/StepContainer.module.css"
 import ImageStep from "./ImageStep"
 import Step from "./Step"
+import useWindowDimensions from "./UseWindowsDimensions"
 
 export default function StepContainer(props) {
+    const { height, width } = useWindowDimensions()
+
     return (
         <div className={Styles.container}>
 
             {props.imageIsLeft ? (
                 <>
                     <div className={Styles.left}>
-                        <ImageStep image={props.image} alt={props.alt} />
+                        {width < 1000 ? (
+                            <Step title={props.title} description={props.description} />
+                        ) : (
+                            <ImageStep image={props.image} alt={props.alt} />
+                        )}
                     </div>
                     <div className={Styles.right}>
-                        <Step title={props.title} description={props.description} />
+                        {width < 1000 ? (
+                            <ImageStep image={props.image} alt={props.alt} />
+                        ) : (
+                            <Step title={props.title} description={props.description} />
+                        )}
                     </div>
                 </>
             ) : (
                 <>
-
-
                     <div className={Styles.left}>
                         <Step title={props.title} description={props.description} />
                     </div>
