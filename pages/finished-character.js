ae62976a260691fee4294b0a92b5d31ffff5a81d
J.A. Aartsen <jamal.aartsen@wearetriple.com>
2022-09-24 16:13:07 +0200

    Add finished character route

---
+++ pages/finished-character.js
@@ -0,0 +1,9 @@
+import Styles from "../styles/FinishedCharacter.module.css"
+
+export default function FinishedCharacter() {
+    return (
+        <div>
+            Finished Character
+        </div>
+    )
+}
